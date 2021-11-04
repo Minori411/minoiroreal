b3f97de5e6037e5316d524eaebd90dd05ad9498e
Minori411 <whmd.030@icloud.com>
2021-11-04 17:30:43 +0900

    rails assets:precomple実行

--- app/javascript/components/Sample.tsx
+++ app/javascript/components/Sample.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Sample: React.FC = () => {
-  return <>this is typescript sampletttttt.</>;
+  return <>this is typescript samplecd.</>;
 };
  
 export default Sample;
